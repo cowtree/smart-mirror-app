36f11328385ed8d23a2d5a2e79d5a632a9d1e557
Cao Tri Do <huycaotrido@gmail.com>
2023-01-29 14:13:02 +0100

    working verison multi container setup with data generator

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Data(BaseModel):
+    value: int
 
 app = FastAPI()
+
+data_storage = []
 
 @app.get("/")
 def read_root():
@@ -10,9 +16,21 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/data")
-def post_data(data: dict):
-    return data
+@app.post("/data")
+async def receive_data(data: Data):
+    # store the data in the data_storage list
+    print(data.dict())
+    data_storage.append(data.dict())
+    return {"message": "Data received"}
 
+@app.get("/data")
+async def get_data():
+    # return the data stored in the data_storage list
+    try:
+        # Return the data from the in-memory store
+        return {"data": data_storage}
+    except Exception as e:
+        logging.error(f"Error getting data: {e}")
+        raise
 
 
--- backend-datagen/data_generator.py
+++ backend-datagen/data_generator.py
@@ -4,6 +4,6 @@ import time
 
 while True:
     data = {"value": random.randint(1, 100)}
-    response = requests.post("http://fastapi:8000/data", json=data)
+    response = requests.post("http://backend-api:8000/data", json=data)
     print(response.status_code, response.json())
     time.sleep(1)
